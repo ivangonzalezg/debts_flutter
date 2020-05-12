3f02868a1bd37fdaf47fdc9351d59307491cafde
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2020-05-12 02:17:00 -0500

    Loading screen on sign in and sign up

--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:debts/screens/auth/sign_up.dart';
 import 'package:debts/services/auth.dart';
 import 'package:debts/shared/constants.dart';
+import 'package:debts/shared/loading.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
@@ -17,94 +18,94 @@ class _SignInScreenState extends State<SignInScreen> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _loading = false;
 
   Future<Null> _onSignIn() async {
+    setState(() => _loading = true);
     try {
       await _authService.signInWithEmailAndPassword(
         _emailController.text.toString(),
         _passwordController.text.toString(),
       );
+      setState(() => _loading = false);
     } catch (error) {
-      _scaffold.currentState.showSnackBar(
-        SnackBar(
-          content: Text(error.toString()),
-          backgroundColor: Colors.red[900],
-          duration: Duration(seconds: 1),
-        ),
-      );
+      setState(() => _loading = false);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffold,
-      appBar: AppBar(
-        title: Text('Sign In'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person_add),
-            onPressed: () =>
-                Navigator.pushNamed(context, SignUpScreen.routeName),
-            label: Text("Sign Up"),
-          )
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.all(28),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  validator: (value) => value.isEmpty
-                      ? 'Please enter a email'
-                      : EmailValidator.validate(value)
-                          ? null
-                          : "Please enter a valid email",
-                  decoration: textInputDecoration.copyWith(
-                    labelText: "Email",
-                    icon: Icon(
-                      Icons.email,
-                    ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  validator: (value) => value.length < 6
-                      ? 'Please enter a password 6+ chars long'
-                      : null,
-                  decoration: textInputDecoration.copyWith(
-                    labelText: "Password",
-                    icon: Icon(
-                      Icons.lock,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
+    return _loading
+        ? Loading()
+        : Scaffold(
+            key: _scaffold,
+            appBar: AppBar(
+              title: Text('Sign In'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  icon: Icon(Icons.person_add),
                   onPressed: () =>
-                      _formKey.currentState.validate() ? _onSignIn() : null,
-                  color: Colors.blue,
-                  child: Text(
-                    "Sign in",
-                    style: TextStyle(color: Colors.white),
-                  ),
+                      Navigator.pushNamed(context, SignUpScreen.routeName),
+                  label: Text("Sign Up"),
                 )
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.all(28),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) => value.isEmpty
+                            ? 'Please enter a email'
+                            : EmailValidator.validate(value)
+                                ? null
+                                : "Please enter a valid email",
+                        decoration: textInputDecoration.copyWith(
+                          labelText: "Email",
+                          icon: Icon(
+                            Icons.email,
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        controller: _passwordController,
+                        validator: (value) => value.length < 6
+                            ? 'Please enter a password 6+ chars long'
+                            : null,
+                        decoration: textInputDecoration.copyWith(
+                          labelText: "Password",
+                          icon: Icon(
+                            Icons.lock,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        onPressed: () => _formKey.currentState.validate()
+                            ? _onSignIn()
+                            : null,
+                        color: Colors.blue,
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
--- lib/screens/auth/sign_up.dart
+++ lib/screens/auth/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:debts/services/auth.dart';
 import 'package:debts/shared/constants.dart';
+import 'package:debts/shared/loading.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
@@ -16,93 +17,101 @@ class _State extends State<SignUpScreen> {
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _loading = false;
 
   Future<Null> _onSignUp() async {
-    await _authService.signUpWithEmailAndPassword(
-      _emailController.text.toString(),
-      _passwordController.text.toString(),
-      _nameController.text.toString(),
-    );
-    Navigator.pop(context);
+    setState(() => _loading = true);
+    try {
+      await _authService.signUpWithEmailAndPassword(
+        _emailController.text.toString(),
+        _passwordController.text.toString(),
+        _nameController.text.toString(),
+      );
+      Navigator.pop(context);
+    } catch (e) {
+      setState(() => _loading = false);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign Up'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(28),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: _nameController,
-                validator: (value) =>
-                    value.isEmpty ? 'Please enter a name' : null,
-                decoration: textInputDecoration.copyWith(
-                  labelText: "Name",
-                  icon: Icon(
-                    Icons.person,
-                  ),
+    return _loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Sign Up'),
+            ),
+            body: Container(
+              padding: EdgeInsets.all(28),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      validator: (value) =>
+                          value.isEmpty ? 'Please enter a name' : null,
+                      decoration: textInputDecoration.copyWith(
+                        labelText: "Name",
+                        icon: Icon(
+                          Icons.person,
+                        ),
+                      ),
+                      textCapitalization: TextCapitalization.words,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) => value.isEmpty
+                          ? 'Please enter a name'
+                          : EmailValidator.validate(value)
+                              ? null
+                              : "Please enter a valid email",
+                      decoration: textInputDecoration.copyWith(
+                        labelText: "Email",
+                        icon: Icon(
+                          Icons.email,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      validator: (value) => value.length < 6
+                          ? 'Please enter a password 6+ chars long'
+                          : null,
+                      decoration: textInputDecoration.copyWith(
+                        labelText: "Password",
+                        icon: Icon(
+                          Icons.lock,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RaisedButton(
+                      onPressed: () =>
+                          _formKey.currentState.validate() ? _onSignUp() : null,
+                      color: Colors.blue,
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
                 ),
-                textCapitalization: TextCapitalization.words,
               ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: _emailController,
-                validator: (value) => value.isEmpty
-                    ? 'Please enter a name'
-                    : EmailValidator.validate(value)
-                        ? null
-                        : "Please enter a valid email",
-                decoration: textInputDecoration.copyWith(
-                  labelText: "Email",
-                  icon: Icon(
-                    Icons.email,
-                  ),
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: _passwordController,
-                validator: (value) => value.length < 6
-                    ? 'Please enter a password 6+ chars long'
-                    : null,
-                decoration: textInputDecoration.copyWith(
-                  labelText: "Password",
-                  icon: Icon(
-                    Icons.lock,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                onPressed: () =>
-                    _formKey.currentState.validate() ? _onSignUp() : null,
-                color: Colors.blue,
-                child: Text(
-                  "Sign up",
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
